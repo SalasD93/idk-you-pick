bac8b1e7c83bded03d4e95355fe4513fa634118b
Diana Salas <salasd10418@gmail.com>
2021-07-26 04:46:14 -0400

    refactor code for css / add function to start button

--- script.js
+++ script.js
@@ -5,12 +5,24 @@ var HoneySingle = ("male or female");
 var body = document.body;
 
 $(document).ready(function(){
+    // This is for the intro paragraph
     var introP = document.createElement('p');
     $(introP).attr('id', "intro");
+    // Add the css class here
+    $(introP).addClass("");
+    // This is for the start button
     var startBtn = document.createElement('button');
     $(startBtn).attr('id', "start");
     introP.innerHTML = "This application allows you to randomly generate a restaurant in your area when you are having a hard time choosing where to eat." + "<br/>" + "To use this app, enter your zip code on the next page and answer the following questions.";
-    startBtn.innerText = "START";
+    // Add the style class for the button to the anchor tag
+    startBtn.innerHTML = `<a>START</a>`;
     body.append(introP);
     body.append(startBtn);
+    function hideIntro() {
+        $(introP).hide();
+        $(startBtn).hide();
+    }
+    $(startBtn).on('click', function () {
+        hideIntro();
+    });
 });
