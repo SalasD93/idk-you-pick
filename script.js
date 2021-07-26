6497d80467e36baab17b0a6b222479b001356cca
Diana Salas <salasd10418@gmail.com>
2021-07-26 04:58:27 -0400

    add intro container / update hideIntro function

--- script.js
+++ script.js
@@ -5,6 +5,11 @@ var HoneySingle = ("male or female");
 var body = document.body;
 
 $(document).ready(function(){
+    // container for intro
+    var introContainer = document.createElement('div');
+    $(introContainer).attr('id', 'intro-container');
+    // add css class for container
+    $(introContainer).addClass("");
     // This is for the intro paragraph
     var introP = document.createElement('p');
     $(introP).attr('id', "intro");
@@ -16,13 +21,15 @@ $(document).ready(function(){
     introP.innerHTML = "This application allows you to randomly generate a restaurant in your area when you are having a hard time choosing where to eat." + "<br/>" + "To use this app, enter your zip code on the next page and answer the following questions.";
     // Add the style class for the button to the anchor tag
     startBtn.innerHTML = `<a>START</a>`;
-    body.append(introP);
-    body.append(startBtn);
+    body.append(introContainer);
+    introContainer.appendChild(introP);
+    introContainer.appendChild(startBtn);
+    // This function will hide the intro so we can display new info
     function hideIntro() {
-        $(introP).hide();
-        $(startBtn).hide();
+        $(introContainer).hide();
     }
     $(startBtn).on('click', function () {
         hideIntro();
     });
+
 });
