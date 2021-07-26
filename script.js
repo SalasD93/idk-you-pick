3897c61e193cc835685bf007ffee3490d9071efe
Diana Salas <salasd10418@gmail.com>
2021-07-26 05:41:25 -0400

    create elements and container for ZIP CODE section / update START button with function

--- script.js
+++ script.js
@@ -21,6 +21,7 @@ $(document).ready(function(){
     introP.innerHTML = "This application allows you to randomly generate a restaurant in your area when you are having a hard time choosing where to eat." + "<br/>" + "To use this app, enter your zip code on the next page and answer the following questions.";
     // Add the style class for the button to the anchor tag
     startBtn.innerHTML = `<a>START</a>`;
+    // Adds elements to HTML
     body.append(introContainer);
     introContainer.appendChild(introP);
     introContainer.appendChild(startBtn);
@@ -28,8 +29,41 @@ $(document).ready(function(){
     function hideIntro() {
         $(introContainer).hide();
     }
+
+    // This is the container for the zip code info
+    var zipContainer = document.createElement('div');
+    $(zipContainer).attr('id', "zip-container");
+    // Add the container class for css here
+    $(zipContainer).attr("");
+    // This is the zip code p
+    var zipP = document.createElement('p');
+    $(zipP).attr('id', "zip-text");
+    // Add css class here
+    $(zipP).addClass("");
+    zipP.innerText = "Please enter your ZIP CODE.";
+    // This is for the text input field for the zip code
+    var zipInput = document.createElement('textarea');
+    $(zipInput).attr('id', "zip-input");
+    // Add css class for input box
+    $(zipInput).addClass("");
+    // This is for the zip code submit button
+    var submitBtn = document.createElement('button');
+    $(submitBtn).attr('id', "zip-button");
+    // Add css class like START button
+    submitBtn.innerHTML = `<a>SUBMIT</a>`
+    // Add elements to HTML
+    body.append(zipContainer);
+    zipContainer.appendChild(zipP);
+    zipContainer.appendChild(zipInput);
+    zipContainer.appendChild(submitBtn);
+    // Hide Zip until Start Button clicked to call showZip function
+    $(zipContainer).hide();
+    function showZip() {
+        $(zipContainer).show()
+    }
     $(startBtn).on('click', function () {
         hideIntro();
+        showZip();
     });
 
 });
