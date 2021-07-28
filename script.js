2e379648f707202b07469ab008b03d55bb6f83f0
Diana Salas <salasd10418@gmail.com>
2021-07-27 21:36:35 -0400

    added questions/answer buttons / cleaned up code / the site now functions

--- script.js
+++ script.js
@@ -1,9 +1,47 @@
-var HoneyGuy = ("male");
-var HoneyGal = ("female");
-var HoneySingle = ("male or female");
-
 var body = document.body;
 
+// This is to display questions and answer buttons
+var questionContainer = document.createElement('div');
+$(questionContainer).attr('id', "question-container");
+// Add css class for question container here
+$(questionContainer).addClass("");
+var question = document.createElement('p');
+$(question).attr('id', "question");
+// Add css class for question text here
+$(question).addClass("");
+var answers = document.createElement('div');
+$(answers).attr('id', "sine");
+// Add css class for question container here
+$(answers).addClass("");
+$(question).text("What are you in the mood for?");
+body.append(questionContainer);
+questionContainer.appendChild(question);
+questionContainer.appendChild(answers);
+
+// This is for the generated restuarant to be displayed
+var displayContainer = document.createElement('div');
+$(displayContainer).attr('id', "display-container");
+// Add css class for display container
+$(displayContainer).addClass("");
+var displayRestaurant = document.createElement('p');
+$(displayRestaurant).attr('id', "restaurant-name");
+// Add css class for restaurant name
+$(displayRestaurant).addClass("");
+var restaurantName = localStorage.getItem("Name");
+// if you are trying to style just the restaurant name use the class restaurant in this span
+displayRestaurant.innerHTML = "Your restaurant is:" + "<br/>" + `<span class="restaurant">${restaurantName}</span>`
+// This is a link for the address to link to directions to the restaurant
+var displayAddress = document.createElement('a');
+$(displayAddress).attr('id', "restaurant-address");
+// Add css class for address link here
+$(displayAddress).addClass("");
+var restaurantAddress = localStorage.getItem("Address");
+$(displayAddress).text(restaurantAddress);
+body.append(displayContainer);
+displayContainer.appendChild(displayRestaurant);
+displayContainer.appendChild(displayAddress);
+
+localStorage.clear();
 $(document).ready(function(){
     // container for intro
     var introContainer = document.createElement('div');
@@ -58,30 +96,6 @@ $(document).ready(function(){
     zipContainer.appendChild(zipInput);
     zipContainer.appendChild(submitBtn);
 
-    // This is for the generated restuarant to be displayed
-    var displayContainer = document.createElement('div');
-    $(displayContainer).attr('id', "display-container");
-    // Add css class for display container
-    $(displayContainer).addClass("");
-    var displayRestaurant = document.createElement('p');
-    $(displayRestaurant).attr('id', "restaurant-name");
-    // Add css class for restaurant name
-    $(displayRestaurant).addClass("");
-    var restaurantName = localStorage.getItem("Name");
-    // if you are trying to style just the restaurant name use the class restaurant in this span
-    displayRestaurant.innerHTML = "Your restaurant is:" + "<br/>" + `<span class="restaurant">${restaurantName}</span>`
-    var displayAddress = document.createElement('a');
-    $(displayAddress).attr('id', "restaurant-address");
-    // Add css class for address link here
-    $(displayAddress).addClass("");
-    var restaurantAddress = localStorage.getItem("Address");
-    $(displayAddress).text(restaurantAddress);
-
-    body.append(displayContainer);
-    displayContainer.appendChild(displayRestaurant);
-    displayContainer.appendChild(displayAddress);
-
-    
     function hideZip() {
         $(zipContainer).hide();
     }
@@ -92,88 +106,95 @@ $(document).ready(function(){
         hideIntro();
         showZip();
     });
+    $(submitBtn).on('click', function () {
+        hideZip();
+        showQuestions();
+        // this calls the API to start
+        getData();
+    });
     function hideRestaurant() {
         $(displayContainer).hide();
     }
-    $(submitBtn).on('click', function () {
-        hideZip();
-        // this calls the API to start
-        // getData();
-    });
+    
     // Hide Zip until Start Button clicked to call showZip function
     hideZip();
+    hideQuestions();
+    hideRestaurant();
 });
+function showQuestions() {
+    $(questionContainer).show();
+}
+function hideQuestions() {
+    $(questionContainer).hide();
+}
+function showRestaurant() {
+    $(displayContainer).show();
+}
 
 // Have a function that compares key words to items in the menu arrays
 // API for restuarants and menus by zip code
-// const api_url = "https://api.documenu.com/v2/restaurants/zip_code/33773?size=20&fullmenu=true&top_cuisines=false&key=fe622a5ac3aa4f64e60a0ab9c844306d";
-// async function getData() {
-//     const response = await fetch(api_url, {
-//         "method": "GET",
-//         "headers": {
-//             "x-api-key": "fe622a5ac3aa4f64e60a0ab9c844306d",
-//             "x-rapidapi-host": "documenu.p.rapidapi.com"
-//         }
-//     });
-//     console.log(response);
+const api_url = "https://api.documenu.com/v2/restaurants/zip_code/33773?size=20&fullmenu=true&top_cuisines=false&key=fe622a5ac3aa4f64e60a0ab9c844306d";
+async function getData() {
+    const response = await fetch(api_url, {
+        "method": "GET",
+        "headers": {
+            "x-api-key": "fe622a5ac3aa4f64e60a0ab9c844306d",
+            "x-rapidapi-host": "documenu.p.rapidapi.com"
+        }
+    });
+    console.log(response);
 
-//     const totalRestaurants = await response.json();
-//     console.log(totalRestaurants);
-//     // zip code is postal_code for restaurant data
-//     // const restZipCode = totalRestaurants.data[1].address.postal_code;
-//     // console.log(restZipCode);
-//     // const restMenu = totalRestaurants.data[1].menus;
-//     // console.log(restMenu);
-//     // const restCuisine = totalRestaurants.data[1].cuisines;
-//     // const menuItems = totalRestaurants.data[1].menus[0].menu_sections[0];
-//     // console.log(menuItems);
+    const totalRestaurants = await response.json();
+    console.log(totalRestaurants);
 
-//     var restCuisine = [];
-//     for (var i = 0; i < totalRestaurants.data.length; i++) {
-//         var currentRestaurant = totalRestaurants.data[i];
-//         restCuisine = restCuisine.concat(currentRestaurant.cuisines);
-//         for (var z = 0; z < currentRestaurant.cuisines.length; z++) {
-//             if (currentRestaurant.cuisines[z] != '') {
-//                 document.querySelector('#sine').innerHTML += `<button class="cuisine">${currentRestaurant.cuisines[z]}</button>`;
-//             }
-//         }
-//     }
-//     console.log(restCuisine);
-
-//     // document.querySelector('#name').innerHTML = restName;
-//     // document.querySelector('#address').innerHTML = restAddress;
-//     // document.querySelector('#sine').innerHTML = restCuisine;
-//     // document.querySelector('#menu').innerHTML = menuItems;
-
-//     // This gets the selected cuisine
-//     $('.cuisine').on('click', function (e) {
-//         selectedCuisine(e.target.innerText);
-//         $('.cuisine').hide();
-//     });
-
-//     // clear all cuisine buttons after button click clear HTML of container
-//     // look through total restaurants list to match picked cuisine with for loop
-//     // show them the restaurant
-//     // maybe add menu items
-
-//     // This function uses the selected cuisine to match with a restaurant / gets restaurant name and address
-//     function selectedCuisine(cuisine) {
-//         console.log(cuisine);
-//         for (var i = 0; i < totalRestaurants.data.length; i++) {
-//             var currentRestaurant = totalRestaurants.data[i];
-//             restCuisine = restCuisine.concat(currentRestaurant.cuisines);
-//             if (currentRestaurant.cuisines.includes(cuisine)) {
-//                 console.log(currentRestaurant.restaurant_name);
-//                 const restName = currentRestaurant.restaurant_name;
-//                 $('#name').text(restName);
-//                 localStorage.setItem("Name", restName);
-//                 console.log(currentRestaurant.address.formatted);
-//                 const restAddress = currentRestaurant.address.formatted;
-//                 $('#address').text(restAddress);
-//                 localStorage.setItem("Address", restAddress);
-//             }
+    // zip code is postal_code for restaurant data
+    // const restMenu = totalRestaurants.data[1].menus;
+    // console.log(restMenu);
+    // const menuItems = totalRestaurants.data[1].menus[0].menu_sections[0];
+    // console.log(menuItems);
     
-//         }
-//     }
-// }
-// getData();
+    // This will get the cuisines and put them in buttons
+    var restCuisine = [];
+    for (var i = 0; i < totalRestaurants.data.length; i++) {
+        var currentRestaurant = totalRestaurants.data[i];
+        restCuisine = restCuisine.concat(currentRestaurant.cuisines);
+        for (var z = 0; z < currentRestaurant.cuisines.length; z++) {
+            if (currentRestaurant.cuisines[z] != '') {
+                document.querySelector('#sine').innerHTML += `<button class="cuisine">${currentRestaurant.cuisines[z]}</button>`;
+            }
+        }
+    }
+    console.log(restCuisine);
+
+    // This gets the selected cuisine
+    $('.cuisine').on('click', function (e) {
+        selectedCuisine(e.target.innerText);
+        $('.cuisine').hide();
+        hideQuestions();
+        showRestaurant();
+    });
+
+    // clear all cuisine buttons after button click clear HTML of container
+    // look through total restaurants list to match picked cuisine with for loop
+    // show them the restaurant
+    // maybe add menu items
+    // This function uses the selected cuisine to match with a restaurant / gets restaurant name and address
+    function selectedCuisine(cuisine) {
+        console.log(cuisine);
+        for (var i = 0; i < totalRestaurants.data.length; i++) {
+            var currentRestaurant = totalRestaurants.data[i];
+            restCuisine = restCuisine.concat(currentRestaurant.cuisines);
+            if (currentRestaurant.cuisines.includes(cuisine)) {
+                console.log(currentRestaurant.restaurant_name);
+                const restName = currentRestaurant.restaurant_name;
+                // $('#name').text(restName);
+                localStorage.setItem("Name", restName);
+                console.log(currentRestaurant.address.formatted);
+                const restAddress = currentRestaurant.address.formatted;
+                // $('#address').text(restAddress);
+                localStorage.setItem("Address", restAddress);
+            }
+    
+        }
+    }
+}
